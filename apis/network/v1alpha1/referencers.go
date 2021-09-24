a6464e094d55ae53eacff3f92c521367019a414c
Oleg Senin <olegsenin@yandex-team.ru>
2021-09-24 17:59:01 +0300

    Remove ForProvider; use camelCase in json tags; add reference in network

---
+++ apis/network/v1alpha1/referencers.go
@@ -0,0 +1,39 @@
+package v1alpha1
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/crossplane/crossplane-runtime/pkg/reference"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+)
+
+func NetworkID() reference.ExtractValueFn {
+	return func(mg resource.Managed) string {
+		s, ok := mg.(*Network)
+		if !ok {
+			return ""
+		}
+		return s.Status.AtProvider.ID
+	}
+}
+
+// ResolveReferences of this Subnet
+func (mg *Subnet) ResolveReferences(ctx context.Context, c client.Reader) error {
+	r := reference.NewAPIResolver(c, mg)
+	rsp, err := r.Resolve(ctx, reference.ResolutionRequest{
+		CurrentValue: mg.Spec.NetworkID,
+		Reference:    mg.Spec.NetworkIDRef,
+		Selector:     mg.Spec.NetworkIDSelector,
+		To:           reference.To{Managed: &Network{}, List: &NetworkList{}},
+		Extract:      NetworkID(),
+	})
+	if err != nil {
+		return errors.Wrap(err, "Spec.ForProvider.NetworkID")
+	}
+	mg.Spec.NetworkID = rsp.ResolvedValue
+	mg.Spec.NetworkIDRef = rsp.ResolvedReference
+	return nil
+}
